34df576ecc1733a1291898b69cf3a1aaa7d69dfc
SakshiBhavikatti <sakshibhavikatti04@gmail.com>
2025-06-30 15:28:55 +0530

    key

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,7 +98,7 @@ ingress {
 
 # Key pair
 resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key-sakshi-v4"
+  key_name   = "deployer-key-sakshi-v5"
   public_key = file("C:/Users/saksh/.ssh/id_rsa.pub")
 }
 
